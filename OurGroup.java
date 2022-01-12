8ad3c7c34dfe6c983f61f0141de00c4fe231ae0c
jittipon <ti_jittipon@hotmail.com>
2022-01-12 15:05:02 +0700

    add Jittipon

--- OurGroup.java
+++ OurGroup.java
@@ -6,8 +6,8 @@ public class OurGroup {
     private Vector<String> groupMembers;
     public OurGroup()
     {
-        groupMembers = new Vector<String>();
-        groupMembers.add("Artid"); //PUT YOUR NAME HERE
+        groupMembers = new Vector<String>();//PUT YOUR NAME HERE
+        groupMembers.add("Jittipon"); //PUT YOUR NAME HERE
     }
     public Vector<String> getGroupMembers () {
         return groupMembers;
