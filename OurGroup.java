8a5666eaf747e8fb25608b9ca690b5a4d6dae1d4
ThanakornLZX <thanakorn.phot@ku.th>
2022-01-25 22:32:35 +0700

    My first group changes to OurGroup.java

--- OurGroup.java
+++ OurGroup.java
@@ -12,7 +12,8 @@ public class OurGroup {
         groupMembers.add("Chanwit");
         groupMembers.add("Jittipon"); //PUT YOUR NAME HERE
         groupMembers.add("Rattanamon"); 
-
+	groupMembers.add("Thanakorn");
+	
     }
     public Vector<String> getGroupMembers () {
         return groupMembers;
